b65a8f5f379fa21f888ca096183210ee2996c8f3
Chen Tsumai <henzumi@gmail.com>
2026-04-21 11:18:55 +0300

    adding switch practice

---
+++ switch.js
@@ -0,0 +1,15 @@
+// in presentation 3 
+
+let number = 2;
+
+switch (number) {
+    case 1:
+        alert(1);
+        break;
+    case 2:
+        alert(2);
+        break;
+    default:
+        alert("not found");
+        break;
+}
